42d7bcec0747d2c149519fc6dd6c454d49572553
Martin Mulder <m.j.mulder@provinciegroningen.nl>
2020-10-01 14:58:45 +0200

    Add support for showChanges

--- src/Api/Misc.php
+++ src/Api/Misc.php
@@ -15,6 +15,34 @@ use CheckPoint\ManagementApi\Tools\Validator;
  */
 class Misc extends Api
 {
+    /**
+     * Get a list of changes between dates or sessions
+     *
+     * @param array $givenParameters
+     *
+     * @return mixed|\Psr\Http\Message\StreamInterface
+     * @throws CheckPointManagementApiException
+     * @link https://sc1.checkpoint.com/documents/latest/APIs/index.html#web/show-changes~v1.6%20
+     *
+     */
+    public function showChanges(array $givenParameters)
+    {
+        $allowedParameters = [
+            'from-date',
+            'from-session',
+            'limit',
+            'offset',
+            'to-date',
+            'to-session',
+            'dereference-group-members',
+            'show-membership',
+            'details-level',
+        ];
+
+        Validator::checkParameters($givenParameters, $allowedParameters);
+
+        return $this->request('show-changes', $givenParameters);
+    }
 
     public function __call($method, $arguments)
     {

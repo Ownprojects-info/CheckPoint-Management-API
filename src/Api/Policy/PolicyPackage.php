a2e28757ccfbc1c017b997119d32b751dcd9756a
Martin Mulder <m.j.mulder@provinciegroningen.nl>
2020-10-01 14:18:32 +0200

    Add support for policies

---
+++ src/Api/Policy/PolicyPackage.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace CheckPoint\ManagementApi\Api\Policy;
+
+
+use CheckPoint\ManagementApi\Abstracts\Api;
+use CheckPoint\ManagementApi\Tools\Validator;
+
+/**
+ * Class PolicyPackage
+ */
+class PolicyPackage extends Api
+{
+    /**
+     * Get the datails of a package
+     *
+     * @param array $givenParameters
+     *
+     * @return mixed|\Psr\Http\Message\StreamInterface
+     * @throws CheckPointManagementApiException
+     * @link https://sc1.checkpoint.com/documents/latest/APIs/index.html#web/show-package~v1.6%20
+     *
+     */
+    public function showPackage(array $givenParameters)
+    {
+        $allowedParameters = [
+            'uid',
+            'name',
+            'details-level',
+        ];
+
+        Validator::checkParameters($givenParameters, $allowedParameters);
+
+        return $this->request('show-package', $givenParameters);
+    }
+}
